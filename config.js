0616f18c3f64098ac8114a803bb446dc69fe0d3c
Carlo <100089228+ciao1092@users.noreply.github.com>
2023-12-30 18:17:28 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var config = {};
 
-config.denon_host = '192.168.1.168';
+config.denon_host = '192.168.1.89';
 config.denon_api = 'http://' + config.denon_host + '/goform/';
-config.webserver_port = 80;
+config.webserver_port = 8080;
 
 module.exports = config;
